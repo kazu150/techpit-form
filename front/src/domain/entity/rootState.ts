16b65610dd8067b939e4d82cc71462e17f328f18
Kazushi Isogai <kazu150@gmail.com>
2020-12-23 22:08:12 +0900

    大学情報追加

--- front/src/domain/entity/rootState.ts
+++ front/src/domain/entity/rootState.ts
@@ -1,6 +1,8 @@
 import { Profile } from './profile';
 import { Colleges } from './college'
+import { ValidationState } from './validation'
 export type RootState = {
     profile: Profile;
     colleges: Colleges;
+    validation: ValidationState;
 };
